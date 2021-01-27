376be5a232972dc9807ad3c9712e3ffb729d2686
Vitaliy Hurskyy <vitaliyhurskyy@outlook.com>
2021-01-27 16:11:11 +0000

    Adicionado modelo para representar um projeto

---
+++ IPG_v2.0/Models/Projetos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IPG_v2._0.Models
+{
+    public class Projetos
+    {
+        public int ProjetoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+    }
+}
